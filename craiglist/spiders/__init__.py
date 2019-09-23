6f0b3028e73f3993108983336ed4f35aa9364e53
Desmend Jetton <desmendjetton@gmail.com>
2019-09-23 16:54:24 -0500

    Started spider for  Craigslist NY listings

--- craiglist/spiders/__init__.py
+++ craiglist/spiders/__init__.py
@@ -1,4 +1,19 @@
 # This package will contain the spiders of your Scrapy project
-#
+# -*- coding: utf-8 -*-
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = "jobs"
+    allowed_domains = ["newyork.craigslist.org"]
+    start_urls = (
+        # specific URL
+        'www.newyork.craigslist.org/search/egr',
+    )
+
+    def parse(self, response):
+        pass
