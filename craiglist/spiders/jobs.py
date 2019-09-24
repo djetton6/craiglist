7fb02f45d19ab3c0c3a83022df974a6bccdebfb2
Desmend Jetton <desmendjetton@gmail.com>
2019-09-23 23:28:41 -0500

    yieded listings for appropriate items.csv file

--- craiglist/spiders/jobs.py
+++ craiglist/spiders/jobs.py
@@ -1,11 +1,22 @@
+# This package will contain the spiders of your Scrapy project
 # -*- coding: utf-8 -*-
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+
 import scrapy
 
 
 class JobsSpider(scrapy.Spider):
-    name = 'jobs'
-    allowed_domains = ['newyork.craigslist.org']
-    start_urls = ['http://newyork.craigslist.org/']
+    name = "jobs"
+    allowed_domains = ["newyork.craigslist.org"]
+    start_urls = (
+        # specific URL
+        "https://newyork.craigslist.org/search/egr",
+    )
 
     def parse(self, response):
-        pass
+        listings = response.xpath(
+            '//a[@class="result-title hdrlnk"]/text()').extract()
+        for listing in listings:
+            yield {'Listing': listing}
