243f0f2444a2d253d8da97193045c77fa6c7fafb
Desmend Jetton <desmendjetton@gmail.com>
2019-09-26 00:10:04 -0500

    added new yield and image and desc data pts

--- craiglist/spiders/jobs.py
+++ craiglist/spiders/jobs.py
@@ -25,9 +25,36 @@ class JobsSpider(scrapy.Spider):
             text = listing.xpath(
                 './/a[@class="result-title hdrlnk"]/text()').extract_first()
 
-            yield {'date': date, 'link': link, 'text': text}
+            yield scrapy.Request(link, callback=self.parse_listing, meta={'date': date, 'link': link, 'text': text})
 
         next_page_url = response.xpath(
             '//a[text()="next > "]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def parse_listing(self, response):
+        date = response.meta['date']
+        link = response.meta['link']
+        text = response.meta['text']
+
+        compensation = response.xpath(
+            '//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
+
+        type = response.xpath(
+            '//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+        images = response.xpath('//*[@id="thumbs"]//@src').extract()
+        images = [image.replace('50x50c', '600x450') for image in images]
+
+        address = response.xpath('//*[@id="postingbody"]/text()').extract()
+
+        yield {
+            'date': date,
+            'link': link,
+            'text': text,
+            'compensation': compensation,
+            'type': type,
+            'images': images,
+            'address': address
+
+        }
